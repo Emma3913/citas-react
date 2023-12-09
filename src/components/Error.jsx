2459d2a3589fa455db7daa6a6d29ff6de0dd7559
Emma3913 <platypusdestroyer003@gmail.com>
2023-12-09 17:21:21 -0600

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = ( {children} ) => {
+  return (
+    <div className="bg-red-800 w-full text-center text-white p-3 uppercase font-bold mb-3 rounded-md">
+      {children}
+    </div>    
+  )
+}
+
+export default Error
